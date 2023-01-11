2a0212cfc8dda34b3a421d0e77dcf7ae359b30ef
ivqueiroga <ivanqueirogap@gmail.com>
2023-01-11 14:35:33 -0300

    Add ColorPicker and wheel and start rework on hooks

--- app/Components/ColorPallet.js
+++ app/Components/ColorPallet.js
@@ -1,54 +1,69 @@
-import React from 'react';
-import { SafeAreaView, FlatList, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { Colors } from '../helpers/ColorData';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import ColorPicker from 'react-native-wheel-color-picker';
 
 import Header from './header';
 import Footer from './footer';
+let color = '#ffffff';
 
 export default function ColorPallet ({ navigation, GlobalState }) {
-  const { setColor, bgOrNumber, setNumberColor, setBrushColor, brushOn, setBrushOn, } = GlobalState;
+  const { ledColor, numberColor, brushColor, setColor, bgOrNumber, setNumberColor, setBrushColor, brushOn, setBrushOn, } = GlobalState;
+  // const [color, setPickerColor] = useState('#ffffff');
 
-  const handleClick = (e) => {
+  useEffect(() => {
     if (brushOn) {
-      setBrushColor(e);
-      setBrushOn(false);
+      color = brushColor;
+    } else if (bgOrNumber) {
+      color = ledColor;
+    } else {
+      color = numberColor;
+    }
+  },[]);
+
+  const onColorChange = (e) => {
+    console.log(e);
+    if (brushOn) {
+      setBrushColor(e, () => {
+        setBrushOn(false);
+      });
+      
     } else if (bgOrNumber) {
       setColor(e);
     } else {
       setNumberColor(e);
     }
-    navigation.goBack();
   }
 
-  const Item = ({ hex }) => (
-    <TouchableOpacity 
-    onPress={() => handleClick(hex)}
-    style={{
-        backgroundColor: hex,
-        height: 60,
-        width: '20%',}
-      }>
-    </TouchableOpacity>
-  );
-
-  const renderItem = ({ item }) => (
-    <Item 
-      style={{ flexDirection: 'column' }} 
-      hex={item.hex}
-    />
-  );
+  const setColorHandle = () => {
+    if (brushOn) {
+      setBrushOn(false);
+    }
+    navigation.goBack()
+  }
 
   return (
     <View style={styles.screen}>
       <Header />
       <View style={styles.body}>
         <SafeAreaView style={styles.container}>
-          <FlatList contentContainerStyle={styles.gridBoard}
-            data={Colors}
-            renderItem={renderItem}
-            keyExtractor={item => item.id}
-            numColumns={5}
-          />
+          <View style={styles.colorPickerContainer}>
+              <ColorPicker
+                color={color}
+                // onColorChange={(color) => onColorChange(color)}
+                onColorChangeComplete={(color) => onColorChange(color)}
+                // onInteractionStart= {color => alert(`Start ${color}`)}
+                thumbSize={30}
+                sliderSize={30}
+                noSnap={true}
+                row={false}
+                swatches={false}
+              />
+          </View>
+          <TouchableOpacity
+            onPress={(color) => setColorHandle(color)}
+            style={styles.button}>
+            <Text style={styles.buttonText}>Set Color</Text>
+          </TouchableOpacity>
         </SafeAreaView>
       </View>
       <Footer navigation={navigation} />
@@ -70,15 +85,39 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    borderColor: 'black',
-  },
-    gridBoard: {
-    height: '100%',
-    justifyContent: 'center',
     alignItems: 'center',
-    flex: 1,
-    flexDirection: "column",
-    padding: 5,
   },
+    colorPickerContainer: {
+    backgroundColor: 'white',
+    width: '80%',
+    height: '80%',
+    borderColor: 'black',
+    borderRadius: 12,
+    padding: 10,
+    marginTop: '10%',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#00BFFF',
+    padding: 15,
+    paddingTop: 10,
+    paddingBottom: 10,
+    margin: 20,
+    borderRadius: 12,
+    width: '80%',
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    },
+  buttonText: {
+      color: 'white',
+      fontWeight: '900',
+      fontSize: 18,
+    },
 });
